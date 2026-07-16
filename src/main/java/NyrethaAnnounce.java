28771746fd0ada46d36c9459086adfe55ce39180
austinrhoielegenine199-maker <austinrhoielegenine199@gmail.com>
2026-07-16 18:26:02 +0800

    Update NyrethaAnnounce.javai

--- src/main/java/NyrethaAnnounce.java
+++ src/main/java/NyrethaAnnounce.java
@@ -1,20 +1,44 @@
 package me.nyrethaannounce;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class NyrethaAnnounce extends JavaPlugin {
 
-    private static NyrethaAnnounce instance;
-
     @Override
     public void onEnable() {
-        instance = this;
-        saveDefaultConfig();
-        getCommand("announce").setExecutor(new AnnounceCommand());
-        getLogger().info("§bNyrethaAnnounce enabled!");
+        getLogger().info("NyrethaAnnounce enabled!");
     }
 
-    public static NyrethaAnnounce getInstance() {
-        return instance;
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if (command.getName().equalsIgnoreCase("announce")) {
+
+            if (!sender.hasPermission("announce.use")) {
+                sender.sendMessage(ChatColor.RED + "You do not have permission to use this command.");
+                return true;
+            }
+
+            if (args.length == 0) {
+                sender.sendMessage(ChatColor.RED + "Usage: /announce <message>");
+                return true;
+            }
+
+            String message = String.join(" ", args);
+
+            getServer().broadcastMessage(
+                    ChatColor.DARK_GRAY + "[" +
+                    ChatColor.AQUA + "ANNOUNCEMENT" +
+                    ChatColor.DARK_GRAY + "] " +
+                    ChatColor.WHITE + message
+            );
+
+            return true;
+        }
+
+        return false;
     }
 }
